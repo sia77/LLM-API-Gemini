79977893c912a47c8e7622de310acf4f72d89fbd
Sia <siavash_p@hotmail.com>
2026-01-08 13:04:38 -0800

    Added Literal and default value to the model definition

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Literal
+from pydantic import BaseModel, Field
 
 
 class HistoryItem(BaseModel):
     """Defines a history item object with 'user' or 'model' roles"""
-    role:str
+    role:Literal["user", "model"]
     text: str
 
 class QueryRequest(BaseModel):
     """Request object"""
     prompt: str
     temperature: float = 0.7
-    history: List[HistoryItem]
+    history: List[HistoryItem] = Field(default_factory=list)
